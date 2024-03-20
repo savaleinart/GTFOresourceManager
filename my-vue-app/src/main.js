ed4892b8dd754f2e941c6da213a54def0ca28878
Savaleinart <savaleinart@gmail.com>
2024-03-20 15:49:18 +0100

    update dist

--- my-vue-app/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import PrimeVue from '../node_modules/primevue/config';
-import './style.css'
-import App from './App.vue'
-
-
-createApp(App).use(PrimeVue).mount('#app')
-
